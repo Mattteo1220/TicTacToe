931812db6753c04306ea27e1b344b8a1e37b3601
matt Mcentire <Msmcentire1220@outlook.com>
2023-04-15 18:37:44 -0600

    Continue styling and starting js functionality

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const X = "X";
+const O = "O";
+let isUserTurn = false;
+let isCompTurn = false;
+let computerScore = 0;
 let userScore = 0;
-let compScore = 0;
 
+function changeTurn(){
+    if(isUserTurn){
+        isUserTurn = false;
+        isCompTurn = true;
+    }
+    else{
+        isUserTurn = true;
+        isCompTurn = false;
+    }
+}
